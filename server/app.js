513acb2e16768754c17403bffa996b6d22727836
Luke Schlangen <lukeschlangen@gmail.com>
2017-03-07 16:23:14 -0600

    fixing static file serving

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 //middleware
-app.use(express.static('public'));
+app.use(express.static('server/public'));
 
 app.listen(port, function() {
   console.log('listening on:', port);
